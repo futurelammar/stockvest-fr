0f433b927e8400ca0abfca3e61b83c10417de4a3
bwala-pete <bwalapeter0@gmail.com>
2026-09-01 15:56:53 +0100

    Update layout.tsx

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -1,4 +1,3 @@
-import Script from "next/script";
 import { SiteHeader } from "@/components/layout/site-header";
 import { SiteFooter } from "@/components/layout/site-footer";
 
@@ -8,20 +7,6 @@ export default function MarketingLayout({ children }: { children: React.ReactNod
       <SiteHeader />
       <main className="flex-1">{children}</main>
       <SiteFooter />
-
-      <Script id="tawk-to" strategy="afterInteractive">
-        {`
-          var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-          (function(){
-            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-            s1.async=true;
-            s1.src='https://embed.tawk.to/6a686d1db6c95f1d4221afef/1jujukf4f';
-            s1.charset='UTF-8';
-            s1.setAttribute('crossorigin','*');
-            s0.parentNode.insertBefore(s1,s0);
-          })();
-        `}
-      </Script>
     </div>
   );
 }
